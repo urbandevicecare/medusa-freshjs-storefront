5763cc279d22772962a74e0b833b133af00683ca
urbandevicecare <urbandevice.care@gmail.com>
2026-07-10 22:32:35 +0000

    fix: resolved checkout crash

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,26 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [fresh(), tailwindcss()],
+  resolve: {
+    alias: {
+      "npm:preact@^10.29.1": "preact",
+      "npm:preact@^10.29.1/hooks": "preact/hooks",
+      "npm:preact@^10.27.2": "preact",
+      "npm:preact@^10.27.2/hooks": "preact/hooks",
+      "npm:@preact/signals@^2.9.0": "@preact/signals",
+      "npm:@preact/signals@^2.5.0": "@preact/signals",
+      "npm:@preact/signals@^2.5.1": "@preact/signals",
+      "npm:@preact/signals@^2.2.1": "@preact/signals",
+      "npm:preact-render-to-string@^6.6.3": "preact-render-to-string",
+      "npm:/preact@^10.29.1": "preact",
+      "npm:/preact@^10.29.1/hooks": "preact/hooks",
+      "npm:/preact@^10.27.2": "preact",
+      "npm:/preact@^10.27.2/hooks": "preact/hooks",
+      "npm:/@preact/signals@^2.9.0": "@preact/signals",
+      "npm:/@preact/signals@^2.5.0": "@preact/signals",
+      "npm:/@preact/signals@^2.5.1": "@preact/signals",
+      "npm:/@preact/signals@^2.2.1": "@preact/signals",
+      "npm:/preact-render-to-string@^6.6.3": "preact-render-to-string",
+    },
+  },
 });
